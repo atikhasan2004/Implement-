4b744c515a3f8866855022c9dd779bbeea4e0a6b
Atik Hasan <saatikhasan5021@gmail.com>
2026-03-11 15:01:40 +0600

    Add files via upload
    x=pow(a,5)
    y=pow(b,5)
    z=pow(c,5)
    A=(x+y+z)

---
+++ Implement.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,x,y,z,A;
+    printf("Enter the value of a: ");
+    scanf("%f",&a);
+    y=pow(a,5);
+    printf("Enter the value of b: ");
+    scanf("%f",&b);
+    y=pow(b,5);
+    printf("Enter the value of c: ");
+    scanf("%f",&c);
+    z=pow(c,5);
+    A=(x+y+z);
+    printf ("Enter the result of A is: %.3f",A);
+    return 0;
+}
